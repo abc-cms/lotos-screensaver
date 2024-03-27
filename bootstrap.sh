bb97fbcc3e4c495198b9ff00197b170931954211
Sasha Kosak <a-kosak-mbx@gmail.com>
2024-03-27 20:33:58 +0000

    Alpha 5

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,7 @@
-sudo apt-get -y install vlc xscreensaver
-pip install poetry
-poetry export -f requirements.txt > requirements.txt
+sudo apt-get -y install xscreensaver libopencv-dev pthon3-opencv
 pip install --no-input -r requirements.txt
 cp ./.xscreensaver ~/
 python configure_xscreensaver.py
 chmod +x ./lotos_saver.py
-systemctl --user enable xscreensaver
+sudo systemctl enable xscreensaver
 xscreensaver -no-splash </dev/null &>/dev/null &
