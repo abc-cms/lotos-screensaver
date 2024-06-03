5754a949180c12de3a9930b52342166d30d80a56
Sasha Kosak <alexander.kosak@my.games>
2024-06-03 08:04:45 +0300

    postinstall.sh script fixed

--- postinstall.sh
+++ postinstall.sh
@@ -3,12 +3,14 @@ if [ ! -d ~/.config/lotos-screensaver ]; then
 fi
 
 mkdir -p ~/.config/systemd/user
-rm -f ~/.config/systemd/user/lotos.service
-rm -f ~/.config/systemd/user/lotos.timer
 
 systemctl --user stop lotos.service
+systemctl --user stop lotos.timer
 systemctl --user disable lotos.service
 systemctl --user disable lotos.timer
+
+rm -f ~/.config/systemd/user/lotos.service
+rm -f ~/.config/systemd/user/lotos.timer
 
 cp -r /usr/local/share/lotos-screensaver/config ~/Documents/
 cp -r /usr/local/share/lotos-screensaver/media ~/.config/lotos-screensaver/
